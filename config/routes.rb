73eba20de637981f380d1d79a542c9cbe0b52744
Ricardo Camilo <39412058+ricardo564@users.noreply.github.com>
2019-10-30 09:52:49 -0300

    excluir comentarios dando erro

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-
   resources :articles do
-    resource :comments
+    resources :comments
   end
 
+  get 'articles/index'
+  get 'welcome/inde'
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
