83a4da7b0f7ce92947b1cb7cedb0a236fae72602
Ricardo Camilo <39412058+ricardo564@users.noreply.github.com>
2019-11-13 13:02:28 -0300

    atualizado
    corrigido inde para index

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   end
 
   get 'articles/index'
-  get 'welcome/inde'
+  get 'welcome/index'
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
